306cf5d220443896b7d60b344c2d607714523a78
WalterGuerra <wsteven00@gmail.com>
2024-04-05 16:51:23 -0400

    add select method and move method to select piece and check valid moves ans then moce piece if valid

--- src/checkers/game.py
+++ src/checkers/game.py
@@ -20,3 +20,27 @@ class Game:
   def reset(self):
     self._init()
 
+  def select(self, row, col):
+    if self.selected:
+      result = self._move(row, col)
+      if not result:
+        self.selected = None
+        self.select(row, col)
+    else:
+      piece = self.board.get_piece(row, col)
+      if piece != 0 and self.piece.color ==self.turn:
+        self.selected = piece
+        self.valid_moves= self.board.get_valid_moves(row, col)
+        return True
+  
+    return False
+  
+  def _move(self, row, col):
+    piece = self.board.get_piece(row, col)
+    if self.selected and piece == 0 and (row, col) in self.valid_moves:
+      self.board.move(self.selected, row, col)
+    else:
+      return False
+    
+    return True
+  
